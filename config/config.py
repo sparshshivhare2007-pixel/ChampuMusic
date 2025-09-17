ede331c69e0f05b26b3dfb2dd50631e40f10851e
sparshshivhare2007-pixel <sparshshivhare2007@gmail.com>
2025-09-17 17:23:24 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -67,7 +67,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheChampu/ChampuMusic",
+    "https://github.com/sparshshivhare2007-pixel/ChampuMusic.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
